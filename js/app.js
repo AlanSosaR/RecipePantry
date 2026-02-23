3e5b0fd5402d3e9f5a7412bc2b9b02eaa43e6cba
Alan Sosa <alansosa225@gmail.com>
2026-02-23 02:31:07 +0000

    logo app

--- js/app.js
+++ js/app.js
@@ -77,7 +77,7 @@ function setupLoginScreen() {
             <div class="auth-bg fade-in">
                 <div class="login-card">
                     <div class="icon-container">
-                        <span class="material-symbols-outlined">menu_book</span>
+                        <img src="assets/icons/icon.svg" alt="RecipeHub Logo" style="width: 48px; height: 48px;">
                     </div>
 
                     <h1>${title}</h1>
