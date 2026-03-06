94976a917c208f84ae9a0994d87229d577b5dc56
Alan Sosa <alansosa225@gmail.com>
2026-03-06 23:56:12 +0000

    UI: Left-aligned minimal stepper (v44)

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,5 +1,5 @@
 const SW_PATH = '/sw.js';
-const APP_VERSION_ID = '43';
+const APP_VERSION_ID = '44';
 
 // AUTO-SYNCHRONIZER: Forzar actualización si el HTML y el JS no coinciden
 (function () {
--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
  * Implementa estrategias de invalidación de caché robustas para producción.
  */
 
-const CACHE_NAME = 'recipehub-v43';
-const BUILD_ID = '2026-03-06-v43';
+const CACHE_NAME = 'recipehub-v44';
+const BUILD_ID = '2026-03-06-v44';
 
 // Recursos esenciales para la App Shell
 const STATIC_RESOURCES = [
